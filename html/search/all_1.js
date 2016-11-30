95ed65ac58c660c0d84920f7ab3308417b6c26cf
JosephFoster118 <Joseph.Foster118@gmail.com>
2016-11-30 00:20:35 -0600

    added stuffs

---
+++ html/search/all_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['dotproduct',['dotProduct',['../class_j118_1_1_math_1_1_vector2_d.html#a6523e255345a0aec99581b1cc92a5f09',1,'J118::Math::Vector2D::dotProduct(Vector2D multiplicand, Vector2D multiplier)'],['../class_j118_1_1_math_1_1_vector2_d.html#a0e8821906e15fd30d245ed0e9b7b1758',1,'J118::Math::Vector2D::dotProduct(Vector2D multiplier)']]]
+];
---
+++ html/search/all_6.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['operator_2a',['operator*',['../class_j118_1_1_math_1_1_vector2_d.html#a1de08c41ec31eda4a11e5c080f83c89f',1,'J118::Math::Vector2D']]],
+  ['operator_2b',['operator+',['../class_j118_1_1_math_1_1_vector2_d.html#a13a3fb9bbc4a7d3fd00c5e0c4d8ebddb',1,'J118::Math::Vector2D']]],
+  ['operator_2d',['operator-',['../class_j118_1_1_math_1_1_vector2_d.html#a28749361a623b6f16ec5308085f6d164',1,'J118::Math::Vector2D']]]
+];
