949ec06a63adc43893352f9f1a2df18ad7d61f93
Joseph Foster <joseph.Foster118@gmail.com>
2020-02-13 22:43:23 -0600

    Set up enviroment script for Rasp Pi Zeros and moved files around

--- cmake_install.cmake
+++ cmake_install.cmake
@@ -37,6 +37,44 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+   "/home/joseph/Software/cross-pi-gcc-9.2.0-0/lib/libJ118.a")
+  if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+file(INSTALL DESTINATION "/home/joseph/Software/cross-pi-gcc-9.2.0-0/lib" TYPE STATIC_LIBRARY FILES "/home/joseph/Software/J118/libJ118.a")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+   "/home/joseph/Software/cross-pi-gcc-9.2.0-0/include/J118/ArgParse.h;/home/joseph/Software/cross-pi-gcc-9.2.0-0/include/J118/Callback.h;/home/joseph/Software/cross-pi-gcc-9.2.0-0/include/J118/Exception.h;/home/joseph/Software/cross-pi-gcc-9.2.0-0/include/J118/IPCallback.h;/home/joseph/Software/cross-pi-gcc-9.2.0-0/include/J118/IPServer.h;/home/joseph/Software/cross-pi-gcc-9.2.0-0/include/J118/Matrix.h;/home/joseph/Software/cross-pi-gcc-9.2.0-0/include/J118/Output.h;/home/joseph/Software/cross-pi-gcc-9.2.0-0/include/J118/Packet.h;/home/joseph/Software/cross-pi-gcc-9.2.0-0/include/J118/PeriodicThread.h;/home/joseph/Software/cross-pi-gcc-9.2.0-0/include/J118/Thread.h;/home/joseph/Software/cross-pi-gcc-9.2.0-0/include/J118/UDPServer.h;/home/joseph/Software/cross-pi-gcc-9.2.0-0/include/J118/UtilityMacros.h;/home/joseph/Software/cross-pi-gcc-9.2.0-0/include/J118/Vector2D.h")
+  if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+file(INSTALL DESTINATION "/home/joseph/Software/cross-pi-gcc-9.2.0-0/include/J118" TYPE FILE FILES
+    "/home/joseph/Software/J118/include/J118/ArgParse.h"
+    "/home/joseph/Software/J118/include/J118/Callback.h"
+    "/home/joseph/Software/J118/include/J118/Exception.h"
+    "/home/joseph/Software/J118/include/J118/IPCallback.h"
+    "/home/joseph/Software/J118/include/J118/IPServer.h"
+    "/home/joseph/Software/J118/include/J118/Matrix.h"
+    "/home/joseph/Software/J118/include/J118/Output.h"
+    "/home/joseph/Software/J118/include/J118/Packet.h"
+    "/home/joseph/Software/J118/include/J118/PeriodicThread.h"
+    "/home/joseph/Software/J118/include/J118/Thread.h"
+    "/home/joseph/Software/J118/include/J118/UDPServer.h"
+    "/home/joseph/Software/J118/include/J118/UtilityMacros.h"
+    "/home/joseph/Software/J118/include/J118/Vector2D.h"
+    )
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
