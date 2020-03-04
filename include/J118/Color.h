8ad3b1ba0765bf9a5c467d8c0a40db48da7460ae
Joseph Foster <joseph.Foster118@gmail.com>
2020-03-04 16:53:24 -0600

    Push for Pi

--- include/J118/Color.h
+++ include/J118/Color.h
@@ -10,11 +10,14 @@ private:
     uint8_t red;
     uint8_t green;
     uint8_t blue;
+    uint8_t alpha;
 public:
-    Color(uint8_t b, uint8_t g, uint8_t r);
+    Color() = delete;
+    Color(uint8_t b, uint8_t g, uint8_t r, uint8_t a = 255);
     Color(uint16_t color);
     virtual ~Color();
     uint16_t getColor565();
+    uint32_t getColorSDL();
     bool operator==(const Color& c);
     uint8_t getRed();
     uint8_t getGreen();
--- src/Graphics/Canvas.cpp
+++ src/Graphics/Canvas.cpp
@@ -18,6 +18,12 @@ Canvas::Canvas(uint32_t width_, uint32_t height_)
         }
     }
 
+    surface = SDL_CreateRGBSurface(0, width, height, 32, 0, 0, 0, 0);
+    
+    printf("Canvas made\n");
+
+
+
 }
 
 
@@ -43,20 +49,21 @@ void Canvas::copyTo656TFT(uint16_t* tft_memory)
 {
     for(auto i = 0; i < 128*128; i++)
     {
-        tft_memory[i] = data[i%width][i/height]->getColor565();
+        uint8_t color[4];
+        memcpy(color,(void*)(surface->pixels) + i*4,4);
+        tft_memory[i] = Color(color[0],color[1],color[2]).getColor565();
     }
 }
 
 void Canvas::drawPolygon(Polygon polygon,Color color)
 {
-    printf("Drawing polygon\n");
     int  nodes, nodeX[360], pixelX, pixelY, i, j, swap ;
     for(i = 0; i < 360; i++)
     {
         nodeX[i] = 10000000;
     }
     int polyCorners = polygon.getPointCount();
-    for (pixelY=0; pixelY<180; pixelY++)
+    for (pixelY=0; pixelY<128; pixelY++)
     {
         nodes=0; j=polyCorners-1;
         for (i=0; i<polyCorners; i++)
@@ -69,19 +76,50 @@ void Canvas::drawPolygon(Polygon polygon,Color color)
             }
             j=i;
 
-            std::sort(std::begin(nodeX),std::end(nodeX));
-            for(int c = 0; c < nodes; c++)
-            {
-                printf("Node[%d] %d\n",c,nodeX[i]);
-            }
+            
         }
+        std::sort(std::begin(nodeX),std::end(nodeX));
+
+        for (i=0; i<nodes; i+=2) {
+        if   (nodeX[i  ]>=128) break;
+        if   (nodeX[i+1]> 0 ) {
+        if (nodeX[i  ]< 0 ) nodeX[i  ]=0 ;
+        if (nodeX[i+1]> 128) nodeX[i+1]=128;
+        for (pixelX=nodeX[i]; pixelX<nodeX[i+1]; pixelX++) *(data[pixelX][pixelY]) = color;
+        }
+        }
+
+
     }
 
     //  Sort the nodes, via a simple “Bubble” sort.
 
 }
 
+void Canvas::clearCanvas(Color color)
+{
+    SDL_LockSurface(surface);
+    SDL_memset(surface->pixels, 0, surface->h * surface->pitch);
 
+    SDL_UnlockSurface(surface);
+}
+
+void Canvas::drawCirle(Point2D<uint32_t> center, Color color)
+{
+    //circleColor (surface,50,50,color.getColorSDL());
+}
+
+void Canvas::drawRect(std::array<Point2D<uint32_t>,2> rect, Color color)
+{
+    SDL_LockSurface(surface);
+    SDL_Rect sdl_rect;
+    sdl_rect.x = rect[0].x;
+    sdl_rect.y = rect[0].y;
+    sdl_rect.w = rect[1].x - rect[0].x;
+    sdl_rect.h = rect[1].y - rect[0].y;
+    SDL_FillRect(surface,&sdl_rect,color.getColorSDL());
+    SDL_UnlockSurface(surface);
+}
 
 }
 
